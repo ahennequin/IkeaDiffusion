9d050913e506de98be679cb5e1dcb357412e94a1
Alexandre <alexandredothennequin@gmail.com>
2025-05-19 09:53:11 +0200

    Set up pipeline to delete duplicates items

--- ikeascraper/pipelines.py
+++ ikeascraper/pipelines.py
@@ -5,9 +5,27 @@
 
 
 # useful for handling different item types with a single interface
+from collections import defaultdict
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
 
 
 class IkeascraperPipeline:
+    """This pipeline deletes duplicates products. Products are deemed duplicates if they both have the same name and image link."""
+
+    item_dict = defaultdict(list)
+
     def process_item(self, item, spider):
+        # Wrap adapter around item, and retrieve needed information
+        adapter = ItemAdapter(item)
+        name = adapter.get("product_name")
+        link = adapter.get("image_link")
+        # If the 'product_name' appears in the dict
+        if name in self.item_dict.keys():
+            # Check if the associated link is listed
+            if link in self.item_dict[name]:
+                raise DropItem(f"Product has already been processed! ({name})")
+
+        # If the item is not present, add it
+        self.item_dict[name].append(link)
         return item
--- ikeascraper/settings.py
+++ ikeascraper/settings.py
@@ -63,8 +63,7 @@ ROBOTSTXT_OBEY = True
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-    #    "ikeascraper.pipelines.IkeascraperPipeline": 300,
-    "scrapy.pipelines.images.ImagesPipeline": 1
+    "ikeascraper.pipelines.IkeascraperPipeline": 1,
 }
 
 IMAGES_STORE = "~/Code/IkeaDiffusion/parsed_data"
